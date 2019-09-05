4ba32f722d74980e85c82a719f0ca1d1a303ee55
John Teague <jcteague@gmail.com>
2019-09-04 21:20:49 -0500

    removed dist

--- packages/efate-uuid/dist/index.d.ts
+++
@@ -1,4 +0,0 @@
-/// <reference path="../global.d.ts" />
-import { BuilderReturnFunction } from 'efate';
-declare const uuidBuilder: (this: string) => BuilderReturnFunction;
-export default uuidBuilder;
