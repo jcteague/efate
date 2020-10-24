9cb3b83fb19f36b74bcdbd2f3220453ce2ae7fe8
John Teague <jcteague@gmail.com>
2020-10-23 23:33:51 -0500

    exposing more builders

--- packages/efate/src/index.ts
+++ packages/efate/src/index.ts
@@ -7,6 +7,11 @@ import asArrayBuilder from './property-builders/array-field-builder';
 import asNumberBuilder from './property-builders/number-field-builder';
 import asBooleanBuilder from './property-builders/boolean-field-builder';
 import asDateBuilder from './property-builders/date-field-builder';
+import asEmailbuilder from './property-builders/email-field-builder';
+import fromFixtureBuilder from './property-builders/from-fixture-field-builder';
+import buildFromFunction from './property-builders/function-field-builder';
+import arrayOfFixtureBuilder from './property-builders/array-of-fixture-builder';
+
 import { asStringBuilder } from './property-builders/string-field-builder';
 
 const propertyBuilders = {
@@ -14,7 +19,11 @@ const propertyBuilders = {
   asArrayBuilder,
   asNumberBuilder,
   asBooleanBuilder,
-  asDateBuilder
+  asDateBuilder,
+  asEmailbuilder,
+  fromFixtureBuilder,
+  buildFromFunction,
+  arrayOfFixtureBuilder
 };
 
 export default Fixture;
