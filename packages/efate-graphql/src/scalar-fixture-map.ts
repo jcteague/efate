60f84c32697f68ecdcc0264cb7955ff782900beb
John Teague <jcteague@gmail.com>
2020-10-23 20:32:51 -0500

    removed graphql plugin

--- packages/efate-graphql/src/scalar-fixture-map.ts
+++
@@ -1,9 +0,0 @@
-import {propertyBuilders} from 'efate';
-import uuidBuilder from "efate-uuid/dist/index";
-export const scalarMap = {
-  'STRING': propertyBuilders.asStringBuilder,
-  'INT': propertyBuilders.asNumberBuilder,
-  'FLOAT': propertyBuilders.asNumberBuilder,
-  'BOOLEAN': propertyBuilders.asBooleanBuilder,
-  'ID': uuidBuilder,
-};
